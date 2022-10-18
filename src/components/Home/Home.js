bd1e4e5213fcd5148fe9d810cc43b2cdf3e11322
Patrick Dantas <patrickdsilva99@gmail.com>
2022-10-17 22:01:36 -0400

    css improvement

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card/Card";
 import classes from "./Home.module.css";
 import NavigationSide from "./NavigationSide";
 
 const Home = () => {
+
+  const views = { welcome: "welcome", ordens: "ordens" };
+  const [activeView, setActiveView] = useState(views.welcome);
+  const changeViewHandler = (selectedView) => {
+    setActiveView(selectedView);
+  };
+
   return (
     <section className={classes.homepage}>
       <div className={classes.sidebar}>
-        <NavigationSide/>
+        <NavigationSide views={views} onChangeView={changeViewHandler}/>
       </div>
       <Card className={classes.home}>
-        <h1>Welcome back!</h1>
+        {activeView === views.welcome && <h1>Welcome back!</h1>}
+        {activeView === views.ordens && <h1>Ordens</h1>}
       </Card>
     </section>
   );
--- src/components/Home/NavigationSide.js
+++ src/components/Home/NavigationSide.js
@@ -1,23 +1,22 @@
-import React, {useContext} from "react";
-import AuthContext from "../../store/auth-context";
+import React from "react";
 import classes from "./NavigationSide.module.css";
 import Card from "../UI/Card/Card";
 
-const NavigationSide = () => {
-  const context = useContext(AuthContext);
+const NavigationSide = (props) => {
+
+  const viewChangeHandler = (event) => {
+    props.onChangeView(event.target.id);
+  }
+
   return (
     <Card className={classes.nav}>
       <ul>
-        {context.isLoggedIn && (
-          <li>
-            <a href="/">Dashboard</a>
-          </li>
-        )}
-        {context.isLoggedIn && (
-          <li>
-            <a href="/">New Order</a>
-          </li>
-        )}
+        <li>
+          <a id={props.views.welcome} onClick={viewChangeHandler}>Home</a>
+        </li>
+        <li>
+          <a id={props.views.ordens} onClick={viewChangeHandler}>Dashboard</a>
+        </li>
       </ul>
     </Card>
   );
