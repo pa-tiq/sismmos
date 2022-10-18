0bff85c73f040efe25dd5df6ea86b3c48aad7394
pa-tiq <patrickdsilva99@gmail.com>
2022-10-18 13:37:40 -0400

    tabela de ordens de serviço

--- src/components/Home/OrderTable.js
+++ src/components/Home/OrderTable.js
@@ -1,9 +1,16 @@
-import React, { useState, useEffect, useCallback} from "react";
+import React, { useState, useEffect, useCallback, Fragment} from "react";
 import classes from "./OrderTable.module.css";
+
+const DUMMY_HEADERS = ['ID', 'Material', 'Última Atualização' ,'Requerente', 'Status', 'Prioridade', 'Tipo'];
+const DUMMY_DATA = [
+    ['1', 'Rádio Falcon NS43','13/03/2022', 'CMA', 'Novo','Alta','Apoio Direto'],
+    ['2', 'Rádio Bear KNS43','13/03/2022', '4CTA', 'Novo','Alta','Apoio Direto'],
+    ['3', 'Rádio Dog LS43','13/03/2022', '3BIS', 'Novo','Alta','Apoio Direto']
+];
 
 const OrderTable = (props) => {
 
-  const [orders, setOrders] = useState();
+  const [orders, setOrders] = useState(DUMMY_DATA);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [sorting, setSorting] = useState({
@@ -72,7 +79,32 @@ const OrderTable = (props) => {
     setSorting({ column, descending });
   }
 
-  return <h1>Ordens</h1>;
+  return (
+    <Fragment>
+      <h1>Ordens de Serviço</h1>
+      <table className={classes.table}>
+      <thead onClick={sort} >
+        <tr>
+          {DUMMY_HEADERS.map((title, idx) => {
+            if(sorting.column == idx){
+              title += sorting.descending ? ' \u2191' : ' \u2193';
+            }
+            return <th className={classes.th} key={idx}>{title}</th>
+          })}
+      </tr>
+      </thead>
+      <tbody className={classes.tbody}>
+        {orders.map((row, idx) => (
+          <tr className={classes.tr} key={idx}>
+          {row.map((cell, idx) => (
+            <td className={classes.td} key={idx}>{cell}</td>
+          ))}
+          </tr>
+      ))}
+      </tbody>
+    </table>
+  </Fragment>
+  );
 };
 
 export default OrderTable;
